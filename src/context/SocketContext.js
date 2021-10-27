348d2ad10bb7ac4704b9c9a71683bd9bc26a0728
Jaiber Flor <jaiver.navia@gmail.com>
2021-10-26 22:42:11 -0500

    socket ok

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -13,7 +13,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online, conectarSocket, desconectarSocket } = useSocket('http://localhost:8080');
+    const { socket, online, conectarSocket, desconectarSocket } = useSocket('https://chatintop.herokuapp.com');
     const { auth } = useContext( AuthContext );
     const { dispatch } = useContext( ChatContext );
 
